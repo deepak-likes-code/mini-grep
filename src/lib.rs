434b9f73447b2694aab3fd5b5cbde81df005c61c
Deepak <deepakkomma@gmail.com>
2023-05-06 13:51:16 +0530

    Refactored the code

--- src/lib.rs
+++ src/lib.rs
@@ -42,12 +42,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Pass in atleast 3 arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get the file path"),
+        };
+
         let is_case_sensitive = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
